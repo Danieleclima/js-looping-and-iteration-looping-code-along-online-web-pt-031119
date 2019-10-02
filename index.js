6753caf2bafba4cb4d3b4d07e49874d5ea836923
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-10-02 22:29:20 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    for (let i = 0; i < array.length; i++){
+        console.log(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+}
+
+function countDown(number){
+    let value = number;
+    while (value >= 0){
+        console.log(value);
+        value - 1;
+    }
+}
